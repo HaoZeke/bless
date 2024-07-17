0499f4b80fec14f1fbc6decfede378280f2cefa7
Rohit Goswami <rog32@hi.is>
2024-07-17 03:31:44 +0000

    MAINT: Finish cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 pub mod runner;
-pub mod storage;
 pub mod storage_backends {
-    pub mod file;
     pub mod gzip;
     pub mod mongodb;
 }
--- src/storage.rs
+++
@@ -1,8 +0,0 @@
-use async_trait::async_trait;
-use tokio::io;
-
-#[async_trait]
-pub trait Storage {
-    async fn save(&self, label: &str, uuid: &str, data: &[String]) -> io::Result<()>;
-    async fn finish(&self) -> io::Result<()>;
-}
